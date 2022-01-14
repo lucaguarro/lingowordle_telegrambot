24d1dff08f40f395b2a293c49952ec6c8a18d22d
Luca Home PC <luca.guarro@gmail.com>
2022-01-13 22:48:24 -0800

    buildpack

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import os
-from dotenv import load_dotenv
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, Defaults
 import commands as c
 
-load_dotenv()
 # API_KEY = os.getenv('API_KEY')
 API_KEY = os.environ.get('API_KEY')
-PORT = int(os.environ.get('PORT', '8443'))
+PORT = int(os.environ.get('PORT', '13978'))
 
 def error(update, context):
     print(f"Update {update} caused error {context.error}")
@@ -17,16 +15,14 @@ def main():
     updater = Updater(API_KEY, use_context=True, defaults=defaults)
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", c.start_command)) # , Filters.entity('spoiler')
+    dp.add_handler(CommandHandler("start", c.start_command))
     dp.add_handler(CallbackQueryHandler(c.button))
-    dp.add_handler(CommandHandler("guess", c.guess_command)) # , Filters.entity('spoiler')
+    dp.add_handler(CommandHandler("guess", c.guess_command))
     dp.add_handler(CommandHandler("status", c.status_command))
     dp.add_handler(CommandHandler("help", c.help_command))
 
-    # dp.add_handler(MessageHandler(Filters.text, handle_message))
     dp.add_error_handler(error)
 
-    # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=API_KEY,
