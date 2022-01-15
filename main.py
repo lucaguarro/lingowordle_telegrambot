db96889de60eb2dda0770bddd294efae061c87c0
Luca Home PC <luca.guarro@gmail.com>
2022-01-15 14:01:44 -0800

    added commands for prod

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ def main():
     dp.add_handler(CallbackQueryHandler(c.button))
     dp.add_handler(CommandHandler("guess", c.guess_command))
     dp.add_handler(CommandHandler("status", c.status_command))
+    dp.add_handler(CommandHandler("addlang", c.add_language_command))
+    dp.add_handler(CommandHandler("dellang", c.del_language_command))
+    dp.add_handler(CommandHandler("seelangs", c.see_languages_command))
     dp.add_handler(CommandHandler("help", c.help_command))
 
     dp.add_error_handler(error)
