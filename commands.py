360a63a68128f1a2b90ffb5a282d6495313342d0
lu_g <luca.guarro@gmail.com>
2022-01-18 11:33:48 -0800

    bug in help command lol

--- commands.py
+++ commands.py
@@ -266,7 +266,7 @@ def status_command(update, context):
 def help_command(update, context):
     update.message.reply_text(
 '''
-/start \[word\] \- \[word\] must be a 4-10 letter word stylized as spoiler text
+/start \[word\] \- \[word\] must be a 4\-10 letter word stylized as spoiler text
 /guess \[word\] \- \[word\] must be a letter word with length of that of the set word
 /status \- outputs all guesses made so far as well as their results
 /addlang \[language\] \- Add a language to the set of allowable words\. English and Italian supported\. If no language is set\, English will be used by default\.
